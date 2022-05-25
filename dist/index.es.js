6a2c31ba4ecde1b530add399c4e7f84140046377
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-05-25 01:30:24 -0400

    def change data strip

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,4 +1,28 @@
-import 'react';
-import 'react-bootstrap/Container';
-import 'react-bootstrap/Navbar';
-import 'react-bootstrap/Nav';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const ZFNavigationBar = ({
+  dark = true,
+  brandName,
+  left,
+  center,
+  right
+}) => {
+  return /*#__PURE__*/React.createElement(Container, {
+    fluid: true
+  }, /*#__PURE__*/React.createElement(Navbar, {
+    bg: dark ? 'dark' : 'light',
+    variant: dark ? 'dark' : 'light',
+    expand: "lg"
+  }, /*#__PURE__*/React.createElement(Container, {
+    fluid: true
+  }, brandName ? /*#__PURE__*/React.createElement(Navbar.Brand, {
+    href: "#"
+  }, brandName) : '', /*#__PURE__*/React.createElement(Navbar.Toggle, null), /*#__PURE__*/React.createElement(Navbar.Collapse, {
+    className: "justify-content-between"
+  }, /*#__PURE__*/React.createElement(Nav, null, left), /*#__PURE__*/React.createElement(Nav, null, center), /*#__PURE__*/React.createElement(Nav, null, right)))));
+};
+
+export { ZFNavigationBar };
